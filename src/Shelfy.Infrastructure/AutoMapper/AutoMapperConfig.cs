271a8024bbb2f37244c428e5ea1c4aa7dd5ecb64
Stolar <stolar28@gmail.com>
2019-04-17 10:21:54 +0200

    Automapper configuration

---
+++ src/Shelfy.Infrastructure/AutoMapper/AutoMapperConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Shelfy.Core.Domain;
+using Shelfy.Infrastructure.DTO;
+
+namespace Shelfy.Infrastructure.AutoMapper
+{
+    public static class AutoMapperConfig
+    {
+        public static IMapper Initialize()
+            => new MapperConfiguration(cfg =>
+                {
+                    cfg.CreateMap<Book, BookDto>();
+                    cfg.CreateMap<User, UserDto>();
+
+                })
+                .CreateMapper();
+
+    }
+}
