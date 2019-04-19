e4cd9ab62ed478813c6ba42510fe2a060983e13e
Stolar <stolar28@gmail.com>
2019-04-19 21:32:45 +0200

    Create Dtos

---
+++ src/Shelfy.Infrastructure/DTO/Author/AuthorDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Shelfy.Infrastructure.DTO.Author
+{
+    public class AuthorDto
+    {
+        public Guid AuthorId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public DateTime? DateOfDeath { get; set; }
+        public string BirthPlace { get; set; }
+        public string AuthorWebsite { get; set; }
+        public string AuthorSource { get; set; }
+    }
+}
---
+++ src/Shelfy.Infrastructure/DTO/Book/BookDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Shelfy.Infrastructure.DTO.Book
+{
+    public class BookDto
+    {
+        public Guid BookId { get; set; }
+        public string Title { get;  set; }
+        public string OriginalTitle { get; set; }
+        public string Description { get;  set; }
+        public string ISBN { get;  set; }
+        public int Pages { get; set; }
+        public string Publisher { get; set; }
+        public DateTime PublishedAt { get; set; }
+        public string ImageUrl { get; protected set; }
+    }
+}
