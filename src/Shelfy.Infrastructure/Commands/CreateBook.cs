518fcfc19e3e81809e8992b8d5ae902f2e112a2e
Stolar <stolar28@gmail.com>
2019-04-25 23:43:26 +0200

    Added commands

---
+++ src/Shelfy.Infrastructure/Commands/CreateBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Shelfy.Infrastructure.Commands
+{
+    public class CreateBook
+    {
+        public string Title { get; set; }
+        public string OriginalTitle { get; set; }
+        public string Description { get; set; }
+        public string ISBN { get; set; }
+        public int Pages { get; set; }
+        public string Publisher { get; set; }
+        public DateTime PublishedAt { get; set; }
+        public IEnumerable<Guid> AuthorsId { get; set;}
+    }
+}
---
+++ src/Shelfy.Infrastructure/Commands/UpdateBook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Shelfy.Infrastructure.Commands
+{
+    public class UpdateBook
+    {
+        public string Title { get; set; }
+        public string OriginalTitle { get; set; }
+        public string Description { get; set; }
+        public string ISBN { get; set; }
+        public int Pages { get; set; }
+        public string Publisher { get; set; }
+        public DateTime PublishedAt { get; set; }
+        public string Cover { get; set; }
+    }
+}
