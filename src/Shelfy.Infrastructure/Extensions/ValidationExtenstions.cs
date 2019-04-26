3834252ba08d8193e49b5779c9a1e6df28c5e706
Stolar <stolar28@gmail.com>
2019-04-26 20:04:35 +0200

    Added ValidationExtensions for book and author update model

---
+++ src/Shelfy.Infrastructure/Extensions/ValidationExtenstions.cs
@@ -0,0 +1,38 @@
+﻿using Shelfy.Core.Domain;
+
+namespace Shelfy.Infrastructure.Extensions
+{
+    public static class ValidationExtensions
+    {
+        /// <summary>
+        /// Validate book in UpdateAsync, after mapping in BookService
+        /// </summary>
+        public static bool IsValid(this Book book)
+        {
+            book.SetTitle(book.Title);
+            book.SetDescription(book.Title);
+            book.SetIsbn(book.ISBN);
+            book.SetPages(book.Pages);
+            book.SetPublisher(book.Publisher);
+
+            return true;
+        }
+
+        /// <summary>
+        /// Validate author before update
+        /// </summary>
+        public static bool IsValid(this Author author)
+        {
+            author.SetFirstName(author.FirstName);
+            author.SetLastName(author.FirstName);
+            author.SetDescription(author.Description);
+            author.SetDateOfBirth(author.DateOfBirth);
+            author.SetDateOfDeath(author.DateOfDeath);
+            author.SetBirthPlace(author.BirthPlace);
+            author.SetAuthorWebsite(author.AuthorWebsite);
+            author.SetAuthorSource(author.AuthorSource);
+
+            return true;
+        }
+    }
+}
