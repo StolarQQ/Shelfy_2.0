3c8220e1d27e382667addd0982156e2f9afde4c2
Stolar <stolar28@gmail.com>
2019-05-02 19:26:27 +0200

    Extension class for default avatar,cover

---
+++ src/Shelfy.Infrastructure/Extensions/DefaultImageExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace Shelfy.Infrastructure.Extensions
+{
+    public static class DefaultImageExtensions
+    {
+        /// <summary>
+        /// If book cover URL is null or whitespace, we set up cover to default one
+        /// </summary>
+        /// <param name="cover"></param>
+        /// <returns></returns>
+        public static string DefaultBookCoverValidation(this string cover)
+        {
+            var defaultCover = "https://www.stolarstate.pl/avatar/book/default.png";
+
+            return cover.IsEmpty() ? defaultCover : cover;
+        }
+        /// <summary>
+        /// If author Image URL is null or whitespace, we set up cover to default one
+        /// </summary>
+        /// <param name="authorImage"></param>
+        /// <returns></returns>
+        public static string DefaultAuthorImageValidation(this string authorImage)
+        {
+            var defaultAuthorImage = "https://www.stolarstate.pl/avatar/author/default.png";
+
+            return authorImage.IsEmpty() ? defaultAuthorImage : authorImage;
+        }
+    }
+}
