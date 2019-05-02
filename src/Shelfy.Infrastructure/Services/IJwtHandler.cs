bdc6c4a51c166dcbba7475038e7d023420fd0257
Stolar <stolar28@gmail.com>
2019-05-02 19:17:27 +0200

    Added Jwt Service

---
+++ src/Shelfy.Infrastructure/Services/IJwtHandler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Shelfy.Core.Domain;
+using Shelfy.Infrastructure.DTO.Jwt;
+
+namespace Shelfy.Infrastructure.Services
+{
+    public interface IJwtHandler
+    {
+        JwtDto CreateToken(Guid userId, Role role);
+    }
+}
---
+++ src/Shelfy.Infrastructure/Services/JwtHandler.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Shelfy.Core.Domain;
+using Shelfy.Infrastructure.DTO.Jwt;
+using Shelfy.Infrastructure.Extensions;
+using JwtRegisteredClaimNames = Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames;
+
+namespace Shelfy.Infrastructure.Services
+{
+    public class JwtHandler : IJwtHandler
+    {
+        private readonly IConfiguration _configuration;
+
+        public JwtHandler(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public JwtDto CreateToken(Guid userId, Role role)
+        {
+            var now = DateTime.UtcNow;
+
+            var claims = new Claim[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, userId.ToString()),
+                new Claim(JwtRegisteredClaimNames.UniqueName, userId.ToString()),
+                new Claim(ClaimTypes.Role, role.ToString()),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Iat, now.ToTimestamp().ToString())
+            };
+
+            var expiryMinutes = double.Parse(_configuration["Jwt:expiryMinutes"]);
+            var expires = now.AddMinutes(expiryMinutes);
+            var signingCredentials =
+                new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:key"])),
+                    SecurityAlgorithms.HmacSha256);
+
+            var jwt = new JwtSecurityToken(
+                issuer: _configuration["Jwt:issuer"],
+                claims: claims,
+                notBefore: now,
+                expires: expires,
+                signingCredentials: signingCredentials
+            );
+
+            var token = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            return new JwtDto
+            {
+                Token = token,
+                Expires = expires.ToTimestamp()
+            };
+        }
+    }
+}
