1086cd5208740aa24a2c425e86e844057b114064
Stolar <stolar28@gmail.com>
2019-05-04 00:21:47 +0200

    Added Review Controller

---
+++ src/Shelfy.API/Controllers/ReviewController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Shelfy.Infrastructure.Commands;
+using Shelfy.Infrastructure.Services;
+
+namespace Shelfy.API.Controllers
+{
+    [Route("book/{bookId}/[controller]")]
+    [ApiController]
+    public class ReviewController : ApiControllerBase
+    {
+        private readonly IReviewService _reviewService;
+
+        public ReviewController(IReviewService reviewService)
+        {
+            _reviewService = reviewService;
+        }
+
+        [HttpPost]
+        [Authorize(Policy = "HasUserRole")]
+        public async Task<IActionResult> Post([FromBody]CreateBookReview command)
+        {
+            await _reviewService.AddAsync(command.Rating, command.Comment, UserId, command.BookId);
+
+            return Created("", command);
+        }
+
+        [HttpDelete("{userId}")]
+        [Authorize(Policy = "HasUserRole")]
+        public async Task<IActionResult> Post([FromBody] Guid bookId)
+        {
+            await _reviewService.DeleteAsync(bookId, UserId);
+
+            return NoContent();
+        }
+    }
+}
