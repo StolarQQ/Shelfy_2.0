1b62054b30382afe2c270da97001824f89606ebf
Stolar <stolar28@gmail.com>
2019-05-04 00:18:27 +0200

    Add validation for review model

--- src/Shelfy.Core/Domain/Review.cs
+++ src/Shelfy.Core/Domain/Review.cs
@@ -22,13 +22,51 @@ namespace Shelfy.Core.Domain
         public Review(Guid reviewId, int rating, string comment, Guid userId, Guid bookId)
         {
             ReviewId = reviewId;
-            Rating = rating;
-            Comment = comment;
+            SetRating(rating);
+            SetComment(comment);
             UserId = userId;
             BookId = bookId;
             CreatedAt = DateTime.UtcNow;
             UpdatedAt = DateTime.UtcNow;
         }
+
+        public void SetRating(int rating)
+        {
+            if (rating < 1)
+            {
+                throw new Exception("Rating cannot be lower than 1");
+            }
+            if (rating > 6)
+            {
+                throw new Exception("Rating cannot be greater than 6");
+            }
+
+            Rating = rating;
+            UpdatedAt = DateTime.UtcNow;
+        }
+
+        public void SetComment(string comment)
+        {
+            if (string.IsNullOrWhiteSpace(comment))
+            {
+                throw new ArgumentException("Comment cannot be empty");
+            }
+
+            if (comment.Length < 5)
+            {
+                throw new ArgumentException("Comment must contain at least 15 characters");
+
+            }
+
+            if (comment.Length > 500)
+            {
+                throw new ArgumentException("Comment cannot contain more than 500 characters");
+            }
+
+            Comment = comment;
+            UpdatedAt = DateTime.UtcNow;
+        }
+
 
         public static Review Create(Guid reviewId, int rating, string comment, Guid userId, Guid bookId)
             => new Review(reviewId, rating, comment, userId, bookId);
