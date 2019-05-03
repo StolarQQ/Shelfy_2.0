0b389cf8c43a1e54d65a2f3c52980689f1615bff
Stolar <stolar28@gmail.com>
2019-05-04 00:21:06 +0200

    Created ReviewService

---
+++ src/Shelfy.Infrastructure/Services/IReviewService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Shelfy.Infrastructure.DTO.Review;
+
+namespace Shelfy.Infrastructure.Services
+{
+    public interface IReviewService
+    {
+        Task<ReviewDto> GetAsync(Guid bookId, Guid reviewId);
+        Task<IEnumerable<ReviewDto>> GetReviewsForBookAsync(Guid bookId);
+        Task AddAsync(int rating, string comment,Guid userId, Guid bookId);
+        Task DeleteAsync(Guid bookId, Guid userId);
+    }
+}
---
+++ src/Shelfy.Infrastructure/Services/ReviewService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.Extensions.Logging;
+using Shelfy.Core.Domain;
+using Shelfy.Core.Repositories;
+using Shelfy.Infrastructure.DTO.Review;
+using Shelfy.Infrastructure.Extensions;
+
+namespace Shelfy.Infrastructure.Services
+{
+    public class ReviewService : IReviewService
+    {
+        private readonly IBookRepository _bookRepository;
+        private readonly ILogger<ReviewService> _logger;
+        private readonly IMapper _mapper;
+
+        public ReviewService(IBookRepository bookRepository, ILogger<ReviewService> logger, IMapper mapper)
+        {
+            _bookRepository = bookRepository;
+            _logger = logger;
+            _mapper = mapper;
+        }
+        
+        public Task<ReviewDto> GetAsync(Guid bookId, Guid reviewId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<ReviewDto>> GetReviewsForBookAsync(Guid bookId)
+        {
+            var book = await _bookRepository.GetOrFailAsync(bookId);
+
+            return _mapper.Map<IEnumerable<ReviewDto>>(book.Reviews);
+        }
+
+        public async Task AddAsync(int rating, string comment, Guid userId, Guid bookId)
+        {
+            var book = await _bookRepository.GetOrFailAsync(bookId);
+            var review = Review.Create(Guid.NewGuid(), rating, comment, userId, bookId);
+            book.AddReview(review);
+
+            _logger.LogInformation($"Review with id '{review.ReviewId}'" +
+                                   $" was created for book '{book.Title}' by user with id '{userId}'");
+        }
+
+        public async Task DeleteAsync(Guid bookId, Guid userId)
+        {
+            var book = await _bookRepository.GetOrFailAsync(bookId);
+            book.DeleteReview(userId);
+
+            _logger.LogInformation($"Review by user {userId} was deleted for book {book.Title}");
+        }
+    }
+}
