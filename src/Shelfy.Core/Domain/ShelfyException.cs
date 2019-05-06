b9ef7f7250b35ddce54fa1fe0f1f2a5ca5c8f7c2
Stolar <stolar28@gmail.com>
2019-05-06 23:53:10 +0200

    Added custom exceptions for domain and service

---
+++ src/Shelfy.Core/Domain/ShelfyException.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Shelfy.Core.Domain
+{
+    public class ShelfyException : Exception
+    {
+        public string Code { get; }
+
+        protected ShelfyException()
+        {
+        }
+
+        protected ShelfyException(string code)
+        {
+            Code = code;
+        }
+
+        protected ShelfyException(string message, params object[] args) : this(string.Empty, message, args)
+        {
+        }
+
+        protected ShelfyException(string code, string message, params object[] args) : this(null, code, message, args)
+        {
+        }
+
+        protected ShelfyException(Exception innerException, string message, params object[] args)
+            : this(innerException, string.Empty, message, args)
+        {
+        }
+
+        protected ShelfyException(Exception innerException, string code, string message, params object[] args)
+            : base(string.Format(message, args), innerException)
+        {
+            Code = code;
+        }
+    }
+}
