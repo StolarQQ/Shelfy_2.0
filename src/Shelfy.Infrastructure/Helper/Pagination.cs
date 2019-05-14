251dc3dc088ff7c4537eae959662be48bd506e13
Stolar <stolar28@gmail.com>
2019-05-14 23:44:56 +0200

    Added pagination model, and helper class

---
+++ src/Shelfy.Infrastructure/Helper/Pagination.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shelfy.Infrastructure.Helper
+{
+    public static class Pagination
+    {
+        public static PagedResult<T> Paginate<T>(this IEnumerable<T> source,
+            int currentPage, int pageSize)
+        {
+            if (source == null || !source.Any())
+                return PagedResult<T>.Empty;
+
+            if (currentPage <= 0)
+                currentPage = 1;
+
+            if (pageSize <= 0)
+                pageSize = 10;
+
+            if (pageSize > 15)
+                pageSize = 15;
+
+            var totalCount = source.Count();
+            var totalPages = (int)Math.Ceiling((double)totalCount / pageSize);
+            var paginatedSource = source.Skip(pageSize * (currentPage - 1)).Take(pageSize);
+
+            return PagedResult<T>.Create(paginatedSource, totalPages, currentPage, pageSize, totalCount);
+        }
+    }
+}
