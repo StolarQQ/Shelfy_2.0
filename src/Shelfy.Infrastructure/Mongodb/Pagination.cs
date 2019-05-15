85f6c919a3ca9d20ec423f711db7ca4b8fff5d2b
Stolar <stolar28@gmail.com>
2019-05-15 10:36:29 +0200

    Added pagination for mongo collections

---
+++ src/Shelfy.Infrastructure/Mongodb/Pagination.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using Shelfy.Core.Types;
+
+namespace Shelfy.Infrastructure.Mongodb
+{
+    public static class Pagination
+    {
+        public static async Task<PagedResult<T>> PaginateAsync<T>(this IMongoQueryable<T> collection,
+            int currentPage = 1, int pageSize = 5)
+        {
+            if (currentPage <= 0)
+                currentPage = 1;
+
+            if (pageSize <= 0)
+                pageSize = 5;
+
+            if (pageSize > 15)
+                pageSize = 15;
+
+            var isEmpty = await collection.AnyAsync() == false;
+            if (isEmpty)
+                return PagedResult<T>.Empty;
+
+            var totalResults = await collection.CountAsync();
+            var totalPages = (int)Math.Ceiling((decimal)totalResults / pageSize);
+            var paginatedSource = await collection.Limit(currentPage, pageSize).ToListAsync();
+
+            return PagedResult<T>.Create(paginatedSource, currentPage, pageSize, totalPages, totalResults);
+        }
+
+        private static IMongoQueryable<T> Limit<T>(this IMongoQueryable<T> collection,
+            int currentPage = 1, int pageSize = 5)
+        {
+            var skip = pageSize * (currentPage - 1);
+            var data = collection.Skip(skip)
+                .Take(pageSize);
+
+            return data;
+        }
+    }
+}
