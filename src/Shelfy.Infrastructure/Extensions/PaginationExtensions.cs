e29a957fb7e996650d8b74a5c9ffebcbb01e4db2
Stolar <stolar28@gmail.com>
2019-05-15 10:37:05 +0200

    Move IEnumerable<T> pagination to Extension folder

---
+++ src/Shelfy.Infrastructure/Extensions/PaginationExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Shelfy.Core.Types;
+
+namespace Shelfy.Infrastructure.Extensions
+{
+    public static class PaginationExtensions
+    {
+        public static PagedResult<T> Paginate<T>(this IEnumerable<T> source,
+            int currentPage, int pageSize)
+        {
+            if (source == null || !source.Any())
+                return PagedResult<T>.Empty;
+
+            if (currentPage <= 0)
+                currentPage = 1;
+
+            if (pageSize <= 0)
+                pageSize = 10;
+
+            if (pageSize > 15)
+                pageSize = 15;
+
+            var totalCount = source.Count();
+            var totalPages = (int)Math.Ceiling((double)totalCount / pageSize);
+            var paginatedSource = source.Skip(pageSize * (currentPage - 1)).Take(pageSize);
+
+            return PagedResult<T>.Create(paginatedSource, totalPages, currentPage, pageSize, totalCount);
+        }
+    }
+}
