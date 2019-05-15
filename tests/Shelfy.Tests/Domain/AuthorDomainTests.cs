c70a7edae9a7ad14277fbd96aef4a70da9170635
Stolar <stolar28@gmail.com>
2019-05-15 15:10:46 +0200

    Added author domain unit tests

--- tests/Shelfy.Tests/Domain/AuthorDomainTests.cs
+++ tests/Shelfy.Tests/Domain/AuthorDomainTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using AutoFixture;
 using FluentAssertions;
 using Shelfy.Core.Domain;
 using Xunit;
@@ -18,6 +19,38 @@ namespace Shelfy.Tests.Domain
             author.SetFirstName(expectedFirstName);
 
             // Assert
+            author.FirstName.Should().BeEquivalentTo(expectedFirstName);
+        }
+
+        [Fact]
+        public void setFirstName_should_assign_correct_firstName_to_author_when_length_is_2()
+        {
+            // Arrange
+            var author = CorrectAuthor();
+            string expectedFirstName = "TX";
+
+
+            // Act
+            author.SetFirstName(expectedFirstName);
+
+
+            //Assert
+            author.FirstName.Should().BeEquivalentTo(expectedFirstName);
+            expectedFirstName.Length.Should().Be(2);
+        }
+
+        [Fact]
+        public void setFirstName_should_assign_correct_firstName_to_author_when_length_is_20()
+        {
+            // Arrange
+            var author = CorrectAuthor();
+            string expectedFirstName = "ABCDEFGHIJKLMNOPRSTU";
+
+            // Act
+            author.SetFirstName(expectedFirstName);
+
+            //Assert
+            expectedFirstName.Length.Should().Be(20);
             author.FirstName.Should().BeEquivalentTo(expectedFirstName);
         }
 
@@ -47,6 +80,102 @@ namespace Shelfy.Tests.Domain
             exception.Message.Should().BeEquivalentTo(expectedExMsg);
         }
 
+        [Fact]
+        public void setFirstName_should_thrown_exception_when_firstName_length_is_smaller_than_2()
+        {
+            // Arrange
+            var author = CorrectAuthor();
+            string expectedFirstName = "T";
+            var expectedExMsg = "FirstName must contain at least 2 characters";
+
+            // Act & Assert
+            var exception = Assert.Throws<ArgumentException>(() => author.SetFirstName(expectedFirstName));
+            exception.Message.Should().BeEquivalentTo(expectedExMsg);
+        }
+
+        [Fact]
+        public void setFirstName_should_thrown_exception_when_firstName_length_is_greater_than_20()
+        {
+            // Arrange
+            var author = CorrectAuthor();
+            string expectedFirstName = "ABCDEFGHIJKLMNOPRSTUWXYZ";
+
+            var expectedExMsg = "FirstName cannot contain more than 20 characters";
+
+            // Act & Assert
+            var exception = Assert.Throws<ArgumentException>(() => author.SetFirstName(expectedFirstName));
+            exception.Message.Should().BeEquivalentTo(expectedExMsg);
+        }
+
+        [Fact]
+        public void setLastName_should_assign_correct_lastName_to_author()
+        {
+            // Arrange
+            var author = CorrectAuthor();
+            var expectedLastName = "Kowalski";
+
+            // Act
+            author.SetLastName(expectedLastName);
+
+            // Assert
+            author.LastName.Should().BeEquivalentTo(expectedLastName);
+        }
+
+        // LastName
+        [Fact]
+        public void setLastName_should_thrown_exception_when_lastName_is_null()
+        {
+            // Arrange
+            var author = CorrectAuthor();
+            string expectedLastName = null;
+            var expectedExMsg = $"Author with {author.AuthorId} cannot have an empty LastName";
+
+            // Act & Assert
+            var exception = Assert.Throws<ArgumentException>(() => author.SetLastName(expectedLastName));
+            exception.Message.Should().BeEquivalentTo(expectedExMsg);
+        }
+
+        [Fact]
+        public void setLastName_should_thrown_exception_when_lastName_is_whitespace()
+        {
+            // Arrange
+            var author = CorrectAuthor();
+            var expectedLastName = "";
+            var expectedExMsg = $"Author with {author.AuthorId} cannot have an empty LastName";
+
+            // Act & Assert
+            var exception = Assert.Throws<ArgumentException>(() => author.SetLastName(expectedLastName));
+            exception.Message.Should().BeEquivalentTo(expectedExMsg);
+        }
+
+        // TODO FIX REGEX
+        [Fact]
+        public void setAuthorWebsite_should_assign_correct_website_url_to_author()
+        {
+            // Arrange
+            var author = CorrectAuthor();
+            var expectedWebsiteUrl = "http://www.jonskeet.com";
+
+            // Act
+            author.SetAuthorWebsite(expectedWebsiteUrl);
+
+            // Assert
+            author.AuthorWebsite.Should().BeEquivalentTo(expectedWebsiteUrl);
+        }
+        
+        [Fact]
+        public void setAuthorWebsite_should_thrown_exception_when_website_url_doesnt_meet_required_criteria()
+        {
+            // Arrange
+            var author = CorrectAuthor();
+            var expectedWebsiteUrl = "zzz.jonskeet.com";
+            var expectedExMsg = $"URL {expectedWebsiteUrl} doesn't meet required criteria";
+            
+            // Act & Assert
+            var exception = Assert.Throws<ArgumentException>(() => author.SetAuthorWebsite(expectedWebsiteUrl));
+            exception.Message.Should().BeEquivalentTo(expectedExMsg);
+        }
+        
         private Author CorrectAuthor()
         {
             var authorId = Guid.NewGuid();
