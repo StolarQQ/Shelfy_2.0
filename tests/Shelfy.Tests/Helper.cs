e8eec5ee823176b7a1c6251c1bee7b69c870cf4c
Stolar <stolar28@gmail.com>
2019-05-16 18:37:30 +0200

    Added helper method for generate random strings

---
+++ tests/Shelfy.Tests/Helper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace Shelfy.Tests
+{
+    public static class Helper
+    {
+        /// <summary>
+        /// Helper method for generating random string with fixed length
+        /// </summary>
+        /// <param name="stringLength"></param>
+        /// <returns></returns>
+        public static string GenerateRandomString(int stringLength)
+        {
+            var rnd = new Random();
+            var sb = new StringBuilder();
+            var randomString = "ACDASDXASLDJASDJASDLJSADBVOHGD*()(&*ASD*&ASDASHDASIHDO";
+
+            for (var i = 0; i < stringLength; i++)
+                sb.Append(randomString[rnd.Next(0, randomString.Length)]);
+
+            return sb.ToString();
+        }
+    }
+}
