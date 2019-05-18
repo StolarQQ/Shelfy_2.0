ca3abb9ef9bb6a7995f2dce72b98c7e70c031c68
Stolar <stolar28@gmail.com>
2019-05-18 23:16:30 +0200

    Added Controller Base for integration tests

---
+++ tests/Shelfy.Tests.EndToEnd/ControllerTestBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using Shelfy.API;
+using Xunit;
+
+namespace Shelfy.Tests.EndToEnd
+{
+    public abstract class ControllerTestsBase : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        protected HttpClient Client { get; }
+
+        protected ControllerTestsBase(WebApplicationFactory<Startup> fixture)
+        {
+            Client = fixture.CreateClient();
+            Client.BaseAddress = new Uri("https://localhost");
+        }
+
+        protected static StringContent GetPayload(object data)
+        {
+            var json = JsonConvert.SerializeObject(data);
+
+            return new StringContent(json, Encoding.UTF8, "application/json");
+        }
+    }
+}
