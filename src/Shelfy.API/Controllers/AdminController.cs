6bcba582beb25b72769f618e748acd140eabba38
Stolar <stolar28@gmail.com>
2019-05-21 17:02:22 +0200

    Added endpoint for seeding data

---
+++ src/Shelfy.API/Controllers/AdminController.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Shelfy.Infrastructure.Services;
+
+namespace Shelfy.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AdminController : ApiControllerBase
+    {
+        private readonly IDataSeeder _dataSeeder;
+
+        public AdminController(IDataSeeder dataSeeder)
+        {
+            _dataSeeder = dataSeeder;
+        }
+        
+        [HttpGet("{seed}")]
+        [Authorize(Policy = "HasAdminRole")]
+        public async Task<IActionResult> Get()
+        {
+            await _dataSeeder.SeedAsync();
+
+            return NoContent();
+        }
+    }
+}
