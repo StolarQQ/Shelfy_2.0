ff9ed3a2481d36994ab9cdb16cf5f512d355879d
Stolar <stolar28@gmail.com>
2019-05-26 13:20:37 +0200

    Fixed ToTimeStamp

--- src/Shelfy.Infrastructure/Extensions/DateTimeExtensions.cs
+++ src/Shelfy.Infrastructure/Extensions/DateTimeExtensions.cs
@@ -12,9 +12,9 @@ namespace Shelfy.Infrastructure.Extensions
         public static long ToTimestamp(this DateTime dateTime)
         {
             var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-            var time = dateTime.Subtract(new TimeSpan(epoch.Ticks));
+            var time = (dateTime.Ticks - epoch.Ticks)/ TimeSpan.TicksPerSecond;
 
-            return time.Ticks / 10000;
+            return time;
         }
     }
 }
