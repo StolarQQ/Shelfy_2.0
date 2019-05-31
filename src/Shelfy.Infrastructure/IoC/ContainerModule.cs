dc080688b9e99d8fb7933e29f821f0821154dbb7
Stolar <stolar28@gmail.com>
2019-05-31 19:06:41 +0200

    Added ContainerModule

---
+++ src/Shelfy.Infrastructure/IoC/ContainerModule.cs
@@ -0,0 +1,26 @@
+﻿using Autofac;
+using Microsoft.Extensions.Configuration;
+using Shelfy.Infrastructure.AutoMapper;
+using Shelfy.Infrastructure.IoC.Modules;
+
+namespace Shelfy.Infrastructure.IoC
+{
+    public class ContainerModule : Autofac.Module
+    {
+        private readonly IConfiguration _configuration;
+
+        public ContainerModule(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterInstance(AutoMapperConfig.Initialize())
+                .SingleInstance();
+            builder.RegisterModule<RepositoryModule>();
+            builder.RegisterModule(new MongoModule(_configuration));
+            builder.RegisterModule<ServiceModule>();
+        }
+    }
+}
